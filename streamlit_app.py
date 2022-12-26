893183fc50868f9021fcc05318162de7699bf0e4
Tegar Levta Anggana <tegarla123@gmail.com>
2022-12-27 05:27:07 +0700

    Tambah halo

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,8 @@ import streamlit as st
 
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
+HALO INI TEGAR
+
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
 forums](https://discuss.streamlit.io).
 
