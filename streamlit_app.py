e993ee19f9553d60b799ea9f12b40eb3d49f8a0b
Tegar Levta Anggana <tegarla123@gmail.com>
2022-12-28 19:51:46 +0700

    mantap

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,20 +8,11 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 """
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-HALO INI TEGAR
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+# Analisis Sentimen terhadap Review Mahasiswa
 """
 
-review = st.text_area(label="Masukkan review (dalam bahasa Inggris):",
-                      placeholder="Contoh: I like this course...")
+input_review = st.text_area(label="Masukkan review (dalam bahasa Inggris):",
+                            placeholder="Contoh: I like this course...")
 
 model = pickle.load(open("Pickle_RL_Model.pkl", 'rb'))
 
@@ -33,8 +24,6 @@ review = review_df.reviews.values
 tokenizer = Tokenizer(num_words=5000)
 tokenizer.fit_on_texts(review)
 sentiment_label = review_df.course_rating.factorize()
-
-# blahblah 2
 
 
 def predict_sentiment(text):
